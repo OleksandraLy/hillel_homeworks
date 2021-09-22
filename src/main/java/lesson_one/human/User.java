6910830bf2a36c6871e4da0cb50c5e5d70c99712
OleksandraLy <ol.lystopad@gmail.com>
2021-09-22 14:51:07 +0300

    lesson four - organized money transaction service from client to client (corrected)

--- src/main/java/lesson_one/human/User.java
+++
@@ -1,7 +0,0 @@
-package lesson_one.human;
-
-public class User extends Human {
-    public User(String name, String surname, int age, String mail, String password) {
-        super(name, surname, age, mail, password, "USER");
-    }
-}
